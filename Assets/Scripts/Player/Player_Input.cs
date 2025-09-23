72bc6b49cee8a5028188c1509255d8e7d2d2248e
Jerry Gu <ncq9fn@virginia.edu>
2025-09-22 22:48:41 -0400

    Renamed file convention

---
+++ Assets/Scripts/Player/Player_Input.cs
@@ -0,0 +1,30 @@
+using System;
+using Global_Utils;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public struct InputPayLoad : INetworkSerializable
+        {
+            public int Tick;
+            public Vector2 InputVector;
+            public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+            {
+                serializer.SerializeValue(ref Tick);
+                serializer.SerializeValue(ref InputVector);
+            }
+        }
+        public InputPayLoad GetInput()
+        {
+            InputPayLoad input = new InputPayLoad
+            {
+                Tick = GlobalTick.Instance.GetCurrentTick(),
+                InputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))
+            };
+            return input;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player_PhysicsSimulation.cs
@@ -0,0 +1,51 @@
+using Global_Utils;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerPhysicsSimulation : MonoBehaviour
+    {
+        [Header("Physics parameters")]
+        public float moveForce = 30f;
+        public float maxControlSpeed = 10f;
+        public float controlInAirMultiplier = 0.75f;
+        public float fallingMultiplier = 15f;
+        
+        [Header("Ground Layer")]
+        public LayerMask groundLayer;
+        
+        private Rigidbody2D _rigidbody2D;
+        
+        public void AddForce(Vector2 force)
+        {
+            _rigidbody2D.AddForce(force, ForceMode2D.Force);
+        }
+
+        public void AddImpulse(Vector2 impulse)
+        {
+            _rigidbody2D.AddForce(impulse, ForceMode2D.Impulse);
+        }
+
+        public void HandleInput(PlayerInput.InputPayLoad playerInput)
+        {
+            Vector2 input =  playerInput.InputVector;
+            bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, .6f, groundLayer);
+            float controlMultiplier = isGrounded ? 1f : controlInAirMultiplier;  // Decrease air horizontal control
+
+            if (_rigidbody2D.linearVelocityY < 0 && !isGrounded)
+            {
+                _rigidbody2D.linearVelocityY -= fallingMultiplier * GlobalTick.Instance.minTimeBetweenTicks / (1f / Time.deltaTime);
+            }
+            
+            // Only add force if we're under max control speed in input direction
+            if (Mathf.Approximately(Mathf.Sign(input.x), Mathf.Sign(_rigidbody2D.linearVelocityX)) &&
+                !(Mathf.Abs(_rigidbody2D.linearVelocityX) < maxControlSpeed)) return;
+            
+            float velocityInInputDir = _rigidbody2D.linearVelocityX * input.x;
+            float forceScale = Mathf.Clamp01(1f - (velocityInInputDir / maxControlSpeed));
+            
+            // I won't lie gpt cooked this weird equation, but it feels good so we move.
+            AddForce(Vector2.right * input * (moveForce * forceScale * controlMultiplier));
+        }
+    }
+}
